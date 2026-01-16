3533dcc578c600baf8f28ed79ee7f58bfade3143
nlp-OETkl <2077752508@qq.com>
2026-01-16 15:47:04 +0800

    Update 机器学习和千问实现文本分类.py

--- 王颜磊/week01/不同模型实现文本分类/机器学习和千问实现文本分类.py
+++ 王颜磊/week01/不同模型实现文本分类/机器学习和千问实现文本分类.py
@@ -22,7 +22,7 @@ model.fit(input_feature, dataset[1].values)
 client = OpenAI(
     # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
     # https://bailian.console.aliyun.com/?tab=model#/api-key
-    api_key="sk-3a4d832ccb3d4b4cae45bc239d0c4b6c", # 账号绑定，用来计费的
+    api_key="sk-3a4d83xxxxxae45bc239d0c4b6c", # 账号绑定，用来计费的
 
     # 大模型厂商的地址，阿里云
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
